adc2e31230f76efb7beb04a4371dd5d8a4532553
akmalluthfi <akmalluthfi19@gmail.com>
2023-08-31 19:57:39 +0700

    add task number 9

--- pertemuan2/tugas.R
+++ pertemuan2/tugas.R
@@ -128,6 +128,24 @@ if (d > 0) {
 
 # No 9 . Program menentukan nilai dalam huruf
 nilai_angka <- sample(0:100, 20, replace = TRUE)
-print(length(nilai_angka))
+nilai_huruf <- c()
 
-# No 11 . Algoritma bubble sort
+for (nilai in nilai_angka) {
+  if (nilai <= 40) {
+    nilai_huruf <- c(nilai_huruf, "E")
+  } else if (nilai >= 41 && nilai <= 55) {
+    nilai_huruf <- c(nilai_huruf, "D")
+  } else if (nilai >= 56 && nilai <= 60) {
+    nilai_huruf <- c(nilai_huruf, "C")
+  } else if (nilai >= 61 && nilai <= 65) {
+    nilai_huruf <- c(nilai_huruf, "BC")
+  } else if (nilai >= 66 && nilai <= 70) {
+    nilai_huruf <- c(nilai_huruf, "B")
+  } else if (nilai >= 71 && nilai <= 80) {
+    nilai_huruf <- c(nilai_huruf, "AB")
+  } else if (nilai >= 81) {
+    nilai_huruf <- c(nilai_huruf, "A")
+  }
+}
+
+print(nilai_huruf)
