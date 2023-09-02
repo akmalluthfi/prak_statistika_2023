5c67de3a40fc038f86524f33eae55e5d5cd97e4a
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-02 20:02:11 +0700

    finish task no.3

---
+++ pertemuan2/1a3.R
@@ -0,0 +1,16 @@
+# No 3 . fungsi seq() dan rep()
+
+x <- seq(-5, 3)
+print(x)
+
+y <- seq(5, 1)
+print(y)
+
+z <- seq(1000000, 2000000, length.out = 7)
+print(z)
+
+p <- rep(c(9, 5), each = 3)
+print(p)
+
+q <- rep(c(1, 2), times = 2, each = 3)
+print(q)
