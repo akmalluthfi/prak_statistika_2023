f6945332f2a70c87bc0819aaa018aa60f2d57b54
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-02 20:01:31 +0700

    finish task no.2

---
+++ pertemuan2/1a2.R
@@ -0,0 +1,16 @@
+# No 2 . Operasi Aritmatika pada Vektor
+a <- c(2, 4, 7, 2, 5, 2, 3, 4, 2, 3)
+b <- c(3, 5, 5, 3, 5, 7, 3, 6, 8, 7)
+
+# Penjumlahan
+print(a + b)
+# Pengurangan
+print(a - b)
+# Perkalian
+print(a * b)
+# Pembagian
+print(a / b)
+# Sisa Hasil Bagi
+print(a %% b)
+# Pangkat
+print(a^b)
