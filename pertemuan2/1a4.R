15ea0c251a1ef893b2d0438f2f9577233a006a04
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-02 23:34:27 +0700

    tidy up task no.4

--- pertemuan2/1a4.R
+++ pertemuan2/1a4.R
@@ -2,19 +2,22 @@
 p <- 2
 l <- 4
 rectangle_area <- p * l
-print(rectangle_area)
+cat("Luas Persegi panjang :", rectangle_area, "\n")
+
 # membuat program luas segitiga
 a <- 3
 t <- 4
 triangle_area <- (1 / 2) * a * t
-print(triangle_area)
+cat("Luas Segitiga :", triangle_area, "\n")
+
 # membuat program volume balok
 p <- 2
 l <- 4
 t <- 5
 block_volume <- p * l * t
-print(block_volume)
+cat("Volume Balok :", block_volume, "\n")
+
 # membuat program volume bola
 r <- 5
 ball_volume <- (4 / 3) * pi * r^3
-print(ball_volume)
+cat("Volume Bola :", ball_volume, "\n")
