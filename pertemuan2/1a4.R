17bde5fc55601cc7b96c22ca738d10873095e17e
akmalluthfi <akmalluthfi19@gmail.com>
2023-09-02 20:02:39 +0700

    finish task no.4

---
+++ pertemuan2/1a4.R
@@ -0,0 +1,20 @@
+# membuat program luas persegi panjang
+p <- 2
+l <- 4
+rectangle_area <- p * l
+print(rectangle_area)
+# membuat program luas segitiga
+a <- 3
+t <- 4
+triangle_area <- (1 / 2) * a * t
+print(triangle_area)
+# membuat program volume balok
+p <- 2
+l <- 4
+t <- 5
+block_volume <- p * l * t
+print(block_volume)
+# membuat program volume bola
+r <- 5
+ball_volume <- (4 / 3) * pi * r^3
+print(ball_volume)
